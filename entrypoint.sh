f32bd486817f76ced6e6301b640fa2573d96ecd0
Bea Sanssi <bs222eh@student.lnu.se>
2025-04-06 18:32:49 +0200

    Update entry point and ci-cd

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,5 @@ echo "Reading environment variables..."
 echo "MONGO_USER: $MONGO_USER"
 echo "MONGO_PASSWORD: $MONGO_PASSWORD"
 
-echo "Restoring MongoDB dump..."
-mongorestore --username "$MONGO_USER" \
-             --password "$MONGO_PASSWORD" \
-             --authenticationDatabase admin \
-             --host mongo \
-             --db graphql-api-db \
-             /docker-entrypoint-initdb.d/graphql-api-db
-
-echo "✅ MongoDB restored"
-
 echo "Starting server..."
 npm start
