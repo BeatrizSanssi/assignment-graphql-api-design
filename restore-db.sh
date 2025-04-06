372f0d25c03304201f3a743d0d55fb004483764d
Bea Sanssi <bs222eh@student.lnu.se>
2025-04-06 22:49:20 +0200

    Update files to use dump. add restore file instead of entrypoint and change container name to mongo again

--- restore-db.sh
+++ restore-db.sh
@@ -4,14 +4,17 @@ set -e
 echo "🔁 Restoring MongoDB from dump..."
 
 # Path to the dump folder
-DUMP_DIR="./db-dump"
-DB_NAME="graphql-api-db"
-
-# Create admin URI
-MONGO_URI="mongodb://${MONGO_USER}:${MONGO_PASSWORD}@localhost:27017/${DB_NAME}?authSource=admin"
+DUMP_DIR=${DUMP_DIR:-./mongodb-seed}
+DB_NAME=${DB_NAME:-graphql-api-db}
 
 # Wait a few seconds to make sure MongoDB container is fully started
 sleep 5
 
 # Run mongorestore inside Mongo container
-docker exec mongo sh -c "mongorestore --username ${MONGO_USER} --password ${MONGO_PASSWORD} --authenticationDatabase admin --drop --db ${DB_NAME} /dump/${DB_NAME}"
+docker compose exec assignment-graphql-api-design-mongo-1 mongorestore \
+  --username "$MONGO_USER" \
+  --password "$MONGO_PASSWORD" \
+  --authenticationDatabase admin \
+  --drop \
+  --db $DB_NAME \
+  $DUMP_DIR/$DB_NAME
