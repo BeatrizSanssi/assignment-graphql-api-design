e44c8a78a55ccbcea71cb87f7481dfbe2707cd49
Bea Sanssi <bs222eh@student.lnu.se>
2025-04-07 10:08:43 +0200

    Debug api step since build is still crashing

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,9 @@ echo "MONGO_PASSWORD: $MONGO_PASSWORD"
 echo "⏳ Waiting for MongoDB..."
 until nc -z mongo 27017; do sleep 1; done
 
+
+echo "✅ Mongo is up!"
+
 if [ "$DOCKER" = "true" ]; then
   echo "Seeding database inside Docker..."
   node seed.js || echo "⚠️ Seeding failed, continuing anyway"
