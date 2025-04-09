023e1db5e8fa690852443e8566267aad1f1fc963
Bea Sanssi <bs222eh@student.lnu.se>
2025-04-09 18:42:28 +0200

    Update movie to not use id

--- src/models/MovieModel.js
+++ src/models/MovieModel.js
@@ -8,16 +8,16 @@ import mongoose from 'mongoose'
 
 // Create a schema.
 const schema = new mongoose.Schema({
-  id: {
-    type: Number,
-    required: true
-    // /**
-    //  * Generates a random number between 100000 and 999999.
-    //  *
-    //  * @returns {number} A random number between 100000 and 999999.
-    //  */
-    // default: () => Math.floor(100000 + Math.random() * 900000)
-  },
+  // id: {
+  //   type: Number,
+  //   required: true
+  //   // /**
+  //   //  * Generates a random number between 100000 and 999999.
+  //   //  *
+  //   //  * @returns {number} A random number between 100000 and 999999.
+  //   //  */
+  //   // default: () => Math.floor(100000 + Math.random() * 900000)
+  // },
   title: {
     type: String,
     required: true
